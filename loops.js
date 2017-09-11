90b0f07a1803a4b93a5aba4d35dcd1c2ea66a703
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:14:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ array = [];
 function forLoop(array) {
 
 
-  for (i = 0; i < 25; i++) {
+  for (let i = 0; i < 25; i++) {
     if (i <= 1) {
       array.push("I am ${i} strange loop${i === 0 ? \'\' : \'s\'}.")
     }
