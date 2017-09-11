ab73a9c65c33815ab5ad3f2fcf1fc9af8594af35
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:30:14 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+array = [];
+
+function forLoop(array) {
+  for (i = 0; i < 25; i++) {
+      array.push('I am ${i} strange loop${i === 0 ? \'\' : \'s\'}.')
+  }
+  return array
+}
+
+function whileLoop(n) {
+
+  while (n > 0) {
+    console.log(--n);
+  }
+  return 'done';
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+    return array
+  } while (array.length > 0 && maybeTrue());
+}
