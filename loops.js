7fde441b6b09421473d9c4b88390633b164b8e5e
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:03:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,9 +5,9 @@ function forLoop() {
 
   for (i = 0; i < 26; i++) {
     if (i <= 1) {
-      console.log('I am' + ${i} + 'strange loop')
+      array.push('I am' + ${i} + 'strange loop')
     } else {
-      console.log('I am' + ${i} + 'strange loops');
+      array.push('I am' + ${i} + 'strange loops');
     }
   }
 }
