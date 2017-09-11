3d0bec8cfb395b53e03a0d428d64a074216ee524
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:12:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,7 +6,8 @@ function forLoop(array) {
   for (i = 0; i < 26; i++) {
     if (i <= 1) {
       array.push("I am ${i} strange loop${i === 0 ? \'\' : \'s\'}.")
+      return array
     }
   }
 }
-return array
+
