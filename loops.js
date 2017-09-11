381407a5222efa964ea67de461f67e29312c0f50
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-11 21:02:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,10 @@ array = [];
 
 function forLoop() {
   var i = 0;
+  
   for (i = 0; i < 26; i++) {
     if (i <= 1) {
-      console.log('I am ${i} strange loop')
+      console.log('I am' + ${i} + 'strange loop')
     } else {
       console.log('I am ${i} strange loops');
     }
